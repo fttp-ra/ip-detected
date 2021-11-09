a206d0d018feae9ae82d37de83827910a204d10f
Roger Manzo <rogermanzo04@gmail.com>
2021-11-09 19:31:13 -0400

    search ip request

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 app.get('/', (req,res) => {
+    let ip = req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
     res.send('Hello world');
+    console.log(ip)
 })
 
 app.listen(port, () => console.log(`Server at http://localhost:${port}`));
