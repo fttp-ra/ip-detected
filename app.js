9252420686dca402cb799df73098222f24063e41
Roger Manzo <rogermanzo04@gmail.com>
2021-11-09 20:05:13 -0400

    see logs for the ip

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 app.get('/', (req,res) => {
-    let ip = req.headers['x-forwarded-for'] ||
-    req.connection.remoteAddress ||
+    /*let ip = req.headers['x-forwarded-for'] ||
+     req.connection.remoteAddress ||
     req.socket.remoteAddress ||
     req.connection.socket.remoteAddress;
     res.send('Hello world');
-    console.log(ip)
+    console.log(ip) */
+    let ip = req.ip
+    console.log(ip);
 })
 
 app.listen(port, () => console.log(`Server at http://localhost:${port}`));
