c1beab2139d74265502ccfff1a2b7c257c069361
Roger Manzo <rogermanzo04@gmail.com>
2021-11-10 16:51:09 -0400

    changes

--- app.js
+++ app.js
@@ -2,13 +2,29 @@ const express = require('express');
 const app =  express();
 const morgan = require('morgan');
 const geoip = require('geoip-lite');
-let fs = require('fs')
-let fileJson = './ip-serch.json'
+let fs = require('fs');
 const port = process.env.PORT || 3000
+const redis = require('redis');
+const portRedis = process.env.PORT || 6379
+/* let redisHost = "redis-19884.c91.us-east-1-3.ec2.cloud.redislabs.com:19884"
+let clientRedis = redis.createClient(portRedis, redisHost) */
+const config = require('./config.json');
+let accountSid = config.TWILIO_ACCOUNT_SID;
+let authToken = config.TWILIO_AUTH_TOKEN;
+const moment = require('moment');
+const clientTwilio = require('twilio')(accountSid, authToken);
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+
+
+/* clientRedis.on('connect', function(err){
+    if(err) return console.log(err.message);
+
+    return console.log('Connected Redist');
+}) */
 
 app.get('/', (req,res) => {
     let ip = req.headers['x-forwarded-for'] ||
@@ -21,26 +37,32 @@ app.get('/', (req,res) => {
 })
 
 app.get('/direction', (req,res) => {
+    res.send('Received parameters');
     let ip = req.headers['x-forwarded-for'] ||
      req.connection.remoteAddress ||
     req.socket.remoteAddress ||
     req.connection.socket.remoteAddress;
-    console.log(ip) 
+    //console.log(ip) 
     let geo = geoip.lookup(ip);
-    console.log(geo);
-    res.send(`<a href="/" class="button">Back</a><br><br> \
-    <br>Ip:${ip} \
-    <br>Country:${geo.country} \
-    <br>Region:${geo.region} \
-    <br>Timezone:${geo.timezone} \
-    <br>City:${geo.city}`);
+    let date = moment().format('LLL')
+    //console.log(geo);
     try {
-        if (fs.existsSync(fileJson)) {
-            console.log('File json has been created!');
-          }
-        } catch(err) {
-        console.error(err)
+        if(fs.existsSync('log.txt')){
+            console.log('File exist');
+            fs.appendFile('log.txt',`\nip->${ip}`, (err) => {
+                if(err) throw err.message;
+
+                console.log('The ip is register in the new file');
+            }) 
+        }else{
+            let logger = fs.createWriteStream('log.txt')
+            console.log('File created');
+            logger.write(`\nip->${ip}`); //date:date});
         }
+
+    } catch (err) {
+        console.log(err.message);
+    }
 })
 
 app.listen(port, () => console.log(`Server at http://localhost:${port}`));
